d4e0ad868eb21912bbf26b61dc1faea705a3a96d
ErickCasGlez <erick.cas.glez@gmail.com>
2022-04-22 14:19:41 -0500

    Cambio de nombre
    Se cambió el nombre para pasar el test

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,7 +1,7 @@
 const MissionCommander = require('./../app/missionCommander');
 describe("Unit test fo mission commander class", () => {
     test('Create a mission commander object', () => {
-      const myMissionCommander = new MissionCommander("Erick")
+      const myMissionCommander = new MissionCommander("Yo")
       expect(myMissionCommander.name).toBe("Yo");
     });
   })
